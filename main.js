5411a5484586812a3fd012971cb68f9f2a3c6bd3
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-01-30 13:02:03 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -10,4 +10,17 @@ Object.isFrozen = () => 1;
     a != "https://note.com/api/v3/trackings/fp" &&
     a != "https://logcollector.note.com/log_tracking_pb.firehose" ?
       fet(a, b) : dummyThen;
+ 
+  let xhr = XMLHttpRequest.prototype;
+  let open = xhr.open;
+  xhr.open = function (a, b, c) {
+    b != "https://note.com/api/v3/trackings/fp" &&
+    b != "https://note.com/api/v2/stats/reading_rate" &&
+    b != "https://note.com/api/v2/stats/read_history" &&
+      open.call(this, a, b, c);
+  }
+  let send = xhr.send;
+  xhr.send = function (a) {
+    this.readyState && send.call(this, a)
+  }
 }
